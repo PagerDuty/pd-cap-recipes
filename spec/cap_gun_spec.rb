6474804ee152d2d222071465553e43e27f6c68e8
Alper Kokmen <alper@pagerduty.com>
2015-01-14 16:15:52 -0800

    get rid of Gemfile.lock
    this is a gem; it should use gemspec.
    
    - remove Gemfile.lock and add it to .gitignore.
    - update capistrano-spec dependency and update tests.

--- spec/cap_gun_spec.rb
+++ spec/cap_gun_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "Cap gun", :recipe => true do
   ["deploy", "deploy:migrations"].each do |task|
     it "should run after #{task}" do
-      after_callbacks_for_task(task).should include('cap_gun:email')
+      expect(after_callbacks_for_task(task)).to include('cap_gun:email')
     end
   end
 end
